6f733b8461b068a7a30e4d3652d782591e466246
OksanaGerzanych <109728377+OksanaGerzanych@users.noreply.github.com>
2023-02-23 18:34:16 +0200

    add files

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const FeedbackOptions = ({ options, onLeaveFeedback }) => (
+  <div>
+    {options.map(option => (
+      <button
+        key={option}
+        type="button"
+        name={option}
+        onClick={onLeaveFeedback}
+      >
+        {option}
+      </button>
+    ))}
+  </div>
+);
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.array,
+  onLeaveFeedback: PropTypes.func,
+};
+export default FeedbackOptions;
